1a10d0b2514836a80512676c1c2ce204c45493da
Dima <skaizy44@gmail.com>
2021-12-07 22:19:44 +0200

    урок 5
    урок 5

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{firstName}}</h1>
+    <h2>{{desc}}</h2>
+    <p>Автомобиль {{obj.car}} имеет скорость: {{obj.speed}} км/час и цвет: {{obj.color}}</p>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: 'App',
+  // components: {
+  //   HelloWorld,
+  // },
+  data: () => ({
+    firstName: 'Dima',
+    desc: 'работоспособность VUE',
+    obj: {
+      car: 'BMW',
+      speed: '220',
+      color: 'black',
+    },
+  }),
 };
 </script>
 
